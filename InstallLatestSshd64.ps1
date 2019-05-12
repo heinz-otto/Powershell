f55147ca964642b0771a37102567e940c32d9f31
Otto <heinz-otto.klas@gmx.de>
2019-05-12 20:47:04 +0200

    Update InstallLatestSshd64.ps1

--- InstallLatestSshd64.ps1
+++ InstallLatestSshd64.ps1
@@ -7,14 +7,20 @@ if ($PSVersionTable.PSVersion.tostring(2) -lt 5.1){
     elseif ($w32caption -match "Windows Server 2012") {$Spattern="W2K12-"}
     else {Write-Output "Setup WMF5 manually";exit}
     write-output "Windows Server 2012 detected $Spattern"
-
-    # The following Code is original from https://gist.github.com/mgreenegit/b80ddd089677f92f56f5
-    # Use shortcode to find latest TechNet download site
-    $confirmationPage = 'http://www.microsoft.com/en-us/download/' +  $((invoke-webrequest 'http://aka.ms/wmf5latest' -UseBasicParsing).links | ? Class -eq 'mscom-link download-button dl' | % href)
-    # Parse confirmation page and look for URL to file
-    $directURL = (invoke-webrequest $confirmationPage -UseBasicParsing).Links | ? Class -eq 'mscom-link' | ? href -match $Spattern | % href | select -first 1
-    write-output "url for WMF 5.1 is $directURL"
-    # Download file to local
+    <#
+        # The following Code is original from https://gist.github.com/mgreenegit/b80ddd089677f92f56f5
+        # Use shortcode to find latest TechNet download site
+        $confirmationPage = 'http://www.microsoft.com/en-us/download/' +  $((invoke-webrequest 'http://aka.ms/wmf5latest' -UseBasicParsing).links | ? Class -eq 'mscom-link download-button dl' | % href)
+        # Parse confirmation page and look for URL to file
+        $directURL = (invoke-webrequest $confirmationPage -UseBasicParsing).Links | ? Class -eq 'mscom-link' | ? href -match $Spattern | % href | select -first 1
+        write-output "url for WMF 5.1 is $directURL"
+        # Download file to local
+        $download = invoke-webrequest $directURL -OutFile $env:Temp\wmf5latest.msu
+    #>
+    # Because the code above don't working on Windows Server 2012, I will using the direkt link instead
+    # May be this linkes will be broken in the future
+    if ($Spattern -eq "W2K12R2-") {$directURL="https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win8.1AndW2K12R2-KB3191564-x64.msu"}
+    elseif ($Spattern -eq "W2K12-"){$directURL="https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/W2K12-KB3191565-x64.msu"}
     $download = invoke-webrequest $directURL -OutFile $env:Temp\wmf5latest.msu
     # Install quietly with no reboot
     write-output "$env:Temp\wmf5latest.msu downloaded"
