66154548623f36f7ced7578b314df070fd783e62
Otto <heinz-otto.klas@gmx.de>
2020-01-29 14:43:31 +0100

    Update GetFilesFromGithub.ps1

--- GetFilesFromGithub.ps1
+++ GetFilesFromGithub.ps1
@@ -1,7 +1,7 @@
 # URL
 $url="https://raw.githubusercontent.com/heinz-otto/scripts/master/batch/"
 # Collection of Files
-$files="StartWinPE.cmd","WinPEmount.cmd","restore.cmd","capture.cmd","FindeLW.cmd","CreatePartitions-UEFI.txt"
+$files="StartWinPE.cmd","WinPEmount.cmd","restore.cmd","capture.cmd","FindeLW.cmd","CreatePartitions-UEFI.txt","CreatePartitions-BIOS.txt"
 # Destination Path if other than actual path
 $dest=""
 foreach($f in $files) {wget -O "$dest$f" "$url$f"}
