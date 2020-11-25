dbf9ce0fe525580fcbafe1725036ac0643020f8c
Otto <heinz-otto.klas@gmx.de>
2020-11-25 15:29:13 +0100

    Update SetupStartWsl.ps1
    Add PS style Help

--- SetupStartWsl.ps1
+++ SetupStartWsl.ps1
@@ -1,5 +1,14 @@
-### This Script will make a special setup for starting WSL and a linux job every time when Windows is starting
-### first create the script for linux job with here string and strip the Windowsstyle newline 
+<#
+.SYNOPSIS
+	This Script will setup start WSL during host startup
+.DESCRIPTION	
+    This Script will make a special setup for starting WSL and a linux job every time when Windows is starting
+.EXAPMLE
+    execute the script only: SetupStartWsl.ps1
+.LINK
+    https://heinz-otto.blogspot.com/2020/11/wsl-windows-linux-wie-macht-man-das.html
+#>
+### 1. create the script for linux job with 'here string' and strip the Windowsstyle newline 
 ###
 $file='c:\scripts\start.sh'
 @"
@@ -7,7 +16,7 @@ cd /opt/fhem
 perl fhem.pl fhem.cfg
 "@ | Out-File -Encoding ASCII $file
 ((Get-Content $file) -join "`n") + "`n" | Set-Content -NoNewline $file
-### second create in a similar way the Powershellscript wich will used from the taskscheduler
+### 2. create in a similar way the Powershellscript wich will used from the taskscheduler
 ###
 $file='c:\scripts\startwsl.ps1'
 @"
@@ -21,13 +30,13 @@ Foreach ($port in $ports){
 Set-NetFirewallRule -DisplayName 'WSL 2 Firewall Unlock' -LocalPort $ports
 "@ | Out-File -Encoding ASCII $file
 ###
-### Create Basic Firewall Rules for the portforwarding to wsl
+### 3. Create Basic Firewall Rules for the portforwarding to wsl
 ###
 $ports=8083,1883
 New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports -Action Allow -Protocol TCP
 New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports -Action Allow -Protocol TCP
 ###
-### Last Step register a Task wich will execute the PS Script every time starting up the Windows Host System
+### Last: register a Task wich will execute the second PS Script every time starting up the Windows Host System
 $userID='otto'
 $taskname='StartWslPP'
 $A = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-command $file"
