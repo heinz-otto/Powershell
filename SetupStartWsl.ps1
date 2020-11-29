6ea9b39d1f59977e72f47436b35708d86a982471
Otto <heinz-otto.klas@gmx.de>
2020-11-29 13:43:29 +0100

    Update SetupStartWsl.ps1

--- SetupStartWsl.ps1
+++ SetupStartWsl.ps1
@@ -1,21 +1,25 @@
 <#
 .SYNOPSIS
-    This Script will setup start WSL during host startup
-.DESCRIPTION	
     This Script will make a special setup for starting WSL and a linux job every time when Windows is starting
+.DESCRIPTION	
+    Prepare usage for the script - there are also some variables inside the script 
+    mkdir c:\scripts -force
+    cd c:\scripts
+    wget -O c:\scripts\SetupStartWsl.ps1 https://raw.githubusercontent.com/heinz-otto/Powershell/master/SetupStartWsl.ps1
+    Set-ExecutionPolicy RemoteSigned
+    .\SetupStartWsl.ps1
 .EXAMPLE
     SetupStartWsl.ps1
 .LINK
     https://heinz-otto.blogspot.com/2020/11/wsl-windows-linux-wie-macht-man-das.html
 #>
 ### Some configuration variables
-$fileS='c:\scripts\start.sh'      # shell scriptname
-$fileP='c:\scripts\startwsl.ps1'  # powershell scriptname
+$fileS='c:\scripts\start.sh'      # shell scriptname - is also hard coded in Step 2
+$fileP='c:\scripts\startwsl.ps1'  # powershell scriptname - must be the full name for Task Action
 $FWRname='WSL Firewall Unlock'    # Firewall Name
 $taskname='StartWslPP'            # Name for Task
-$ports=8083,1883                  # ports for first Rule
-#$userID=(Get-LocalUser |where enabled).name           # UserID for Task 
-$userID=whoami                    # UserID for Task 
+$ports=8083,1883                  # ports for the first Rule
+$userID=whoami                    # UserID for Task - will simple use the current user
 
 ### 1. create the script for linux job with 'here string' and strip the Windowsstyle newline 
 ###
@@ -31,9 +35,9 @@ else
 fi
 '@ | Out-File -Encoding ASCII $fileS
 ((Get-Content $fileS) -join "`n") + "`n" | Set-Content -NoNewline $fileS
+
 ### 2. create in a similar way the Powershellscript wich will used from the taskscheduler
 ###
-
 @'
 wsl -u fhem -d debian -e bash /mnt/c/scripts/start.sh
 $ports=8083,1883                   # Array mit allen Ports
@@ -49,7 +53,7 @@ Set-NetFirewallRule -LocalPort $ports
 '@ + @"
 -DisplayName "$FWRname"
 "@ | Out-File -Encoding ASCII $fileP
-###
+
 ### 3. Create Basic Firewall Rules for the portforwarding to wsl
 ###
 if (-not(Get-NetFirewallRule -DisplayName "$FWRname*")){
@@ -60,7 +64,7 @@ if (-not(Get-NetFirewallRule -DisplayName "$FWRname*")){
 ### Last: register a Task wich will execute the second PS Script every time starting up the Windows Host System
 $A = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-command $fileP"
 $T = New-ScheduledTaskTrigger -AtStartup
-$P = New-ScheduledTaskPrincipal -UserID $userID -LogonType S4U -Id Author -RunLevel Highest
+$P = New-ScheduledTaskPrincipal -UserID $userID -LogonType S4U -RunLevel Highest
 $S = New-ScheduledTaskSettingsSet
 $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
 if (-not(Get-ScheduledTask "$taskname*")) {Register-ScheduledTask "$taskname" -InputObject $D} else {
