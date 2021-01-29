401bcbfec9604310e3b93d4e1a14b91011abd930
Otto <heinz-otto.klas@gmx.de>
2021-01-29 12:00:38 +0100

    Add files via upload

---
+++ ssh-copy-id.ps1
@@ -0,0 +1,48 @@
+﻿<#
+.SYNOPSIS
+    ssh-copy-id for Powershell
+.DESCRIPTION
+    copy the piblic key to the host
+.EXAMPLE
+    ssh-copy-id user@host
+    ssh-copy-id user@host win
+    ssh-copy-id -i .ssh\id_rsa.pub user@host
+    ssh-copy-id -i .ssh\id_rsa.pub user@host win
+.NOTES
+    Some notes
+#>
+
+#region Params
+  param
+  (
+    [Parameter(Mandatory=$false)]
+    $ziel,$os = '',$i = "$env:userprofile\.ssh\id_rsa.pub"
+  )
+#endregion 
+write-verbose "Destination: $ziel"
+write-verbose "PublicKeyLocation: $i"
+write-verbose "DestinationOS: $os"
+
+$pkey = $(type $i) 
+
+if ($os -eq '' -or $os -ne 'win') {
+$cmd=@"
+if ! grep -q `"`"$pkey`"`" .ssh/authorized_keys
+then
+  mkdir -p .ssh
+  echo $pkey >> .ssh/authorized_keys
+else
+  echo 'Key always exist'
+fi
+"@
+} else {
+  $cmd="findstr /c:`"`"$pkey`"`" .ssh\authorized_keys||mkdir .ssh ||echo $pkey >>.ssh\authorized_keys"
+}
+
+write-verbose "complete cmd: $cmd"
+if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent) {
+  ssh $ziel $cmd.replace("`r`n","`n")
+}
+else {
+  ssh $ziel $cmd.replace("`r`n","`n") 2>&1>$null
+}
