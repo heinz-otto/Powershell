f402db45f9a93e6d13e9c3da870a1d6db11fbcb3
Otto <heinz-otto.klas@gmx.de>
2021-01-29 21:26:14 +0100

    Update ssh-copy-id.ps1

--- ssh-copy-id.ps1
+++ ssh-copy-id.ps1
@@ -5,6 +5,7 @@
     copy the piblic key to the host
 .EXAMPLE
     ssh-copy-id user@host
+    ssh-copy-id user@host -path /etc/dropbear   #e.g. for OpenWrt
     ssh-copy-id user@host win
     ssh-copy-id -i .ssh\id_rsa.pub user@host
     ssh-copy-id -i .ssh\id_rsa.pub user@host win
@@ -15,8 +16,10 @@
 #region Params
   param
   (
+    [Parameter(Mandatory=$true)]
+    $ziel,
     [Parameter(Mandatory=$false)]
-    $ziel,$os = '',$i = "$env:userprofile\.ssh\id_rsa.pub"
+    $os = '',$i = "$env:userprofile\.ssh\id_rsa.pub",$path='.ssh'
   )
 #endregion 
 write-verbose "Destination: $ziel"
@@ -27,16 +30,16 @@ $pkey = $(type $i)
 
 if ($os -eq '' -or $os -ne 'win') {
 $cmd=@"
-if ! grep -q `"`"$pkey`"`" .ssh/authorized_keys
+if ! grep -q `"`"$pkey`"`" $path/authorized_keys
 then
-  mkdir -p .ssh
-  echo $pkey >> .ssh/authorized_keys
+  mkdir -p $path
+  echo $pkey >> $path/authorized_keys
 else
   echo 'Key always exist'
 fi
 "@
 } else {
-  $cmd="findstr /c:`"`"$pkey`"`" .ssh\authorized_keys||mkdir .ssh ||echo $pkey >>.ssh\authorized_keys"
+  $cmd="findstr /c:`"`"$pkey`"`" $path\authorized_keys||mkdir $path ||echo $pkey >>$path\authorized_keys"
 }
 
 write-verbose "complete cmd: $cmd"
