598b3d3a81a079731ab639fbef2a2132e02b10f4
Otto <heinz-otto.klas@gmx.de>
2022-02-18 15:38:45 +0100

    Create start-process-as-admin.ps1

---
+++ start-process-as-admin.ps1
@@ -0,0 +1,15 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+$zielnetz='192.168.12.0'
+$router='192.168.56.27'
+$prog="http://192.168.12.1"
+# Route aktivieren
+route add $zielnetz mask 255.255.255.0 $router
+# Anwendung starten, 
+Start-Process $prog -wait
+# viele Anwendungen beenden den Aufrufprozess - dann diese Zeile aktivieren
+Read-Host -Prompt "Route ins Netzwerk $zielnetz noch aktiv - eine Taste zum löschen" #pause
+# Route löschen 
+route delete $zielnetz mask 255.255.255.0 $router
