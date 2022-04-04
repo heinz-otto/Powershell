1aa2aefd01eea790b35fa169f51549fd17797c09
Otto <heinz-otto.klas@gmx.de>
2022-04-04 12:22:54 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,6 @@
-#Get-ChildItem 'c:\'
-$u='https://raw.githubusercontent.com/heinz-otto/Powershell/master/ChangeVariableInsideScript.ps1'
-$f=[System.IO.Path]::GetTempFileName()+'.ps1'
-Invoke-WebRequest -Uri $u -outfile $f
-$f
-PowerShell.exe -ExecutionPolicy Bypass -File $f
-Remove-Item $f
+Get-ChildItem 'c:\'
+# $u='https://raw.githubusercontent.com/heinz-otto/Powershell/master/ChangeVariableInsideScript.ps1'
+# $f=[System.IO.Path]::GetTempFileName()+'.ps1'
+# Invoke-WebRequest -Uri $u -outfile $f
+# PowerShell.exe -ExecutionPolicy Bypass -File $f
+# Remove-Item $f
