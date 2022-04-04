d07ad39db4f48705378d7c84b1f71c86c79a4ba8
Otto <heinz-otto.klas@gmx.de>
2022-04-04 13:44:57 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
+# powershell -nop -c (Invoke-WebRequest -Uri https://raw.githubusercontent.com/heinz-otto/Powershell/master/test.ps1).content|powershell -
 #Get-ChildItem 'c:\'
 $u='https://raw.githubusercontent.com/heinz-otto/Powershell/master/ChangeVariableInsideScript.ps1'
 $f=[System.IO.Path]::GetTempFileName()+'.ps1'
